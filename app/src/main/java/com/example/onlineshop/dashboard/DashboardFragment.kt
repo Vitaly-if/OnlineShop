1d70722d0583e3c192d36feff61c0c62cf26707a
Adminius <vnakvasin@yandex.ru>
2023-03-14 09:21:49 +0700

    fix DashboardFragment.kt

--- app/src/main/java/com/example/onlineshop/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/onlineshop/dashboard/DashboardFragment.kt
@@ -12,8 +12,6 @@ class DashboardFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -27,10 +25,6 @@ class DashboardFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-//        val textView: TextView = binding.textDashboard
-//        dashboardViewModel.text.observe(viewLifecycleOwner) {
-//            textView.text = it
-//        }
         return root
     }
 
